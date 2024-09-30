519d97c7cf1ec50478d7ff2a3fb0c9dbdcb97177
AbdallahErikat <abdallaherikat11@gmail.com>
2024-09-30 14:02:30 +0300

    Update PersonService.java

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -40,14 +40,7 @@ public class PersonService {
 		p.setLastName("Mahmoud");
 		persons.add(p);
 
-		p = new Person();
-		p.setId("5");
-		p.setAge(25);
-		p.setFirstName("webhook");
-		p.setLastName("testing");
-		persons.add(p);
-		
-		
+	
 	}
 	public Person getPerson(String id) {
 		for(Person person:persons) {
